9c335d6d337081ca78b6960a1cb294b200b48ead
Rbandoy <reneboy.andoy@e-horizon.com>
2023-12-31 07:14:22 +0800

    add build

---
+++ app/build/static/js/583.2f3c6126.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[583],{3583:(e,t,l)=>{l.r(t),l.d(t,{default:()=>s});l(2791);var a=l(184);function s(){return(0,a.jsxs)("div",{className:"text-center mt-8 p-5 bg-primary w-full h-full items-center justify-center flex flex-col",children:[(0,a.jsx)("h1",{className:"text-4xl text-gray-800",children:"\ud83d\udea7 Under Construction \ud83d\udea7"}),(0,a.jsx)("p",{className:"text-lg text-gray-600",children:"We are working on this page. It will be available soon!"})]})}}}]);
+//# sourceMappingURL=583.2f3c6126.chunk.js.map
